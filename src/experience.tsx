24f38109a020bf15572274536c581eafbbee5633
Dominic Prior <dominicprior42@gmail.com>
2025-08-25 17:14:26 +0100

    WIP - trying to use OrbitControls

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,8 +1,25 @@
-import { useFrame } from "@react-three/fiber"
+// @ts-nochec k
+
+import type { ThreeElement } from "@react-three/fiber"
+import { useThree, extend, useFrame,  } from "@react-three/fiber"
 import { useRef } from "react"
-import { Mesh } from 'three'
+import { Mesh, Group } from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+extend({ OrbitControls });
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       'orbitControls': ThreeElement<OrbitControls, typeof OrbitControls>;
+//     }
+//   }
+// }
 
 export default function Experience() {
+    const { camera, gl } = useThree();
+    console.log(gl)
     const groupRef = useRef<Group>(null);
     const sphereRef = useRef<Mesh>(null);
     useFrame((_state, delta) => {
@@ -10,6 +27,7 @@ export default function Experience() {
         groupRef!.current!.rotation.x += 0.01 * delta * 60;
     })
     return <>
+        <orbitControls args={ [ camera, gl.domElements ] } />
         <group ref={groupRef}>
         {/* <mesh scale={[3,2,1]} > */}
             <mesh ref={sphereRef} scale={2} rotation-z={0.3}>
