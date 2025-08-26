c8dc5b7d3fb6144165a3ac462858e4d0d49dde6d
Dominic Prior <dominicprior42@gmail.com>
2025-08-26 13:27:28 +0100

    Move the camera

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import Experience from './experience.tsx'
 export default function App() {
 
     return (
-        <Canvas>
+        <Canvas
+            camera={ {
+                fov: 45,
+                position: [6, 6, 6],
+            } }
+        >
             <Experience />
         </Canvas>
     )
